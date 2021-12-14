b2459b564fccfde950daaba5ecd2fdb86cb40763
Alvaro Cobo J <alvarojcobo@gmail.com>
2021-12-13 19:39:39 -0500

    Cambios en el Inicio realizado

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -2,13 +2,73 @@ import React from 'react';
 
 const Index = () => {
   return (
-    <div>
-      <h1>Proyectos Recientes!</h1>
-      <div className='bg-white-300 h-96'>Index page</div>
-      <div className='bg-green-300 h-96'>Index page</div>
-      <div className='bg-green-300 h-96'>Index page</div>
-      <div className='bg-green-300 h-96'>Index page</div>
+<div className="card mb-12">
+  <img className="card-img-top" src="https://www.misiontic2022.gov.co/746/propertyvalues-187848_banner.jpg" alt="Card image cap"></img>
+  <div className="card-body">
+    <h1 className="card-title">7Trenta Administraor de Proyectos</h1>
+    <h3 className="card-text">Planificación estratégica y ejecución más inteligente</h3>
+    <p className="card-text"><small className="text-muted">Proyecto Misión TIC 2021</small></p>
+  </div>
+  <div className="row">
+  <div className="col-sm-2">
+    <div className="card">
+      <div className="card-body">
+      <img className="card-img-top" src="https://cdn.icon-icons.com/icons2/1879/PNG/512/iconfinder-3-avatar-2754579_120516.png" alt="Card image cap"></img>
+      <h5 className="card-title d-flex justify-content-center">DANIEL MONTOYA</h5>
+        <p className="card-text">Scrum Master</p>
+       </div>
     </div>
+  </div>
+
+  <div className="col-sm-2">
+    <div className="card">
+      <div className="card-body">
+      <img className="card-img-top" src="https://cdn-icons-png.flaticon.com/512/194/194938.png" alt="Card image cap"></img>
+        <h5 className="card-title d-flex justify-content-center">PAOLA SIERRA</h5>
+        <p className="card-text">Analista</p>
+       </div>
+    </div>
+  </div>
+  <div className="col-sm-2">
+    <div className="card">
+      <div className="card-body">
+      <img className="card-img-top" src="https://cdn.icon-icons.com/icons2/1879/PNG/512/iconfinder-3-avatar-2754579_120516.png" alt="Card image cap"></img>
+      <h5 className="card-title d-flex justify-content-center">ÁLVARO COBO</h5>
+        <p className="card-text">Desarrollador</p>
+       </div>
+    </div>
+  </div>
+  <div className="col-sm-2">
+    <div className="card">
+      <div className="card-body">
+      <img className="card-img-top" src="https://cdn.icon-icons.com/icons2/1879/PNG/512/iconfinder-3-avatar-2754579_120516.png" alt="Card image cap"></img>
+      <h5 className="card-title d-flex justify-content-center">PABLO LEDESMA</h5>
+        <p className="card-text">Admin BD</p>
+       </div>
+    </div>
+  </div>
+  <div className="col-sm-2">
+    <div className="card">
+      <div className="card-body">
+      <img className="card-img-top" src="https://cdn.icon-icons.com/icons2/1879/PNG/512/iconfinder-3-avatar-2754579_120516.png" alt="Card image cap"></img>
+      <h5 className="card-title d-flex justify-content-center">JHOAN SIERRA</h5>
+        <p className="card-text">Product Onwer</p>
+       </div>
+    </div>
+  </div>
+  <div className="col-sm-2">
+    <div className="card">
+      <div className="card-body">
+      <img className="card-img-top" src="https://cdn.icon-icons.com/icons2/1879/PNG/512/iconfinder-3-avatar-2754579_120516.png" alt="Card image cap"></img>
+      <h5 className="card-title d-flex justify-content-center">JOSÉ CORREDOR</h5>
+        <p className="card-text">Desarrollador</p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
   );
 };
 
